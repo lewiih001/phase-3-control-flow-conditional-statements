3ee0de7eabbe2d2649f1b84194ed3bddd28ade94
lewiih001 <lewismunyambu@gmail.com>
2022-10-21 01:20:50 +0300

    completed assignment

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,52 @@
-def admin_login(username, password)
+def admin_login (username, password)
   # your code here
+  if (username == 'admin' || username == 'ADMIN') && password == '12345'
+     'access granted'
+     
+    else 'access denied'
+    end
 end
 
 def hows_the_weather(temperature)
   # your code here
+  response = if temperature < 40 
+    'brisk'
+    elsif temperature >= 40 && temperature <= 65
+      'a little chilly'
+    elseif temperature > 85
+    'too dang hot'
+    else
+      'perfect'
+    end
+    'it's #{response out there!}'
 end
 
 def fizzbuzz(num)
   # your code here
+  if num % 3 == 0 && num % == 0
+    'fizzbuzz'
+    elsif num % == 0
+      'fizz'
+      elsif num  % == 0
+        'buzz'
+      else 
+        num
+      end
 end
 
 def calculator(operation, num1, num2)
   # your code here
+  case operation
+    when '+'
+      num1 + num2
+      when '-'
+        num1 - num2
+        when '*'
+          num1 * num2
+          when '/'
+            num1 / num2
+            else 
+              puts 'invalid operation!'
+            end
 end
 
