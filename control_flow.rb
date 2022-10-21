18f5ce3efc27f3d739e4660c7d57b0ce120aa52a
lewiih001 <lewismunyambu@gmail.com>
2022-10-21 09:41:46 +0300

    completed lab

--- control_flow.rb
+++ control_flow.rb
@@ -1,52 +1,103 @@
-def admin_login (username, password)
-  # your code here
-  if (username == 'admin' || username == 'ADMIN') && password == '12345'
-     'access granted'
+# def admin_login (username, password)
+#   # your code here
+#   if (username == "admin" || username == "ADMIN") && password == "12345"
+#      "Access granted"
      
-    else 'access denied'
-    end
+#     else
+#        "Access denied"
+#     end
+# end  
+
+
+# # def hows_the_weather(temperature)
+# #   # your code here
+# #   response = if temperature < 40 
+# #     'brisk'
+# #     elsif temperature >= 40 && temperature <= 65
+# #       'a little chilly'
+# #     elseif temperature > 85
+# #     'too dang hot'
+# #     else
+# #       'perfect'
+# #     end
+# #     "it's  #{response} out there!"
+# # end
+
+# # def fizzbuzz(num)
+# #   # your code here
+# #   if num % 3 == 0 && num % == 0
+# #     'fizzbuzz'
+# #     elsif num % == 0
+# #       'fizz'
+# #       elsif num  % == 0
+# #         'buzz'
+# #       else 
+# #         num
+# #       end
+# # end
+
+# # def calculator(operation, num1, num2)
+# #   # your code here
+# #   case operation
+# #     when '+'
+# #       num1 + num2
+# #       when '-'
+# #         num1 - num2
+# #         when '*'
+# #           num1 * num2
+# #           when '/'
+# #             num1 / num2
+# #             else 
+# #               puts 'invalid operation!'
+# #             end
+# # end
+
+def admin_login(username, password)
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    "Access granted"
+  else
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
-  response = if temperature < 40 
-    'brisk'
-    elsif temperature >= 40 && temperature <= 65
-      'a little chilly'
-    elseif temperature > 85
-    'too dang hot'
-    else
-      'perfect'
-    end
-    'it's #{response out there!}'
+  response =  if temperature < 40
+                "brisk"
+              elsif temperature >= 40 && temperature <= 65
+                "a little chilly"
+              elsif temperature > 85
+                "too dang hot"
+              else
+                "perfect"
+              end
+
+  "It's #{response} out there!"
 end
 
 def fizzbuzz(num)
-  # your code here
-  if num % 3 == 0 && num % == 0
-    'fizzbuzz'
-    elsif num % == 0
-      'fizz'
-      elsif num  % == 0
-        'buzz'
-      else 
-        num
-      end
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
   case operation
-    when '+'
-      num1 + num2
-      when '-'
-        num1 - num2
-        when '*'
-          num1 * num2
-          when '/'
-            num1 / num2
-            else 
-              puts 'invalid operation!'
-            end
+  when "+"
+    num1 + num2
+  when "-"
+    num1 - num2
+  when "*"
+    num1 * num2
+  when "/"
+    num1 / num2
+  else
+    puts "Invalid operation!"
+  end
 end
 
